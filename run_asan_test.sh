deb2fddb53cb99f121a066bc4d38ce9b0731e931
Hao Wu <hwu27@gmu.edu>
2025-12-22 00:28:35 -0500

    Revert "Add ASAN test wrapper to match CI subprocess behavior"
    This reverts commit 3933b55f519514973474e2ba64db33d07e95e426.

--- run_asan_test.sh
+++ run_asan_test.sh
@@ -24,8 +24,22 @@ export LD_LIBRARY_PATH=$(find /opt -wholename *lib/asan/libamdhip64.so -printf '
 export CLANG_ASAN_LIB=$(find /opt -name libclang_rt.asan-x86_64.so)
 export HIP_ASAN_LIB=$(find /opt -wholename *lib/asan/libamdhip64.so)
 
-# Environment variables are set in test_asan_wrapper.py (matching CI behavior)
-# The wrapper runs test_asan.py in a subprocess
+export HSA_DISABLE_FRAGMENT_ALLOCATOR=1
+export AMD_PYTORCH_NO_CUDA_MEMORY_CACHING=1
+export PYTORCH_NO_HIP_MEMORY_CACHING=1
+export TRITON_ENABLE_ASAN=1
+
+# HSA_XNACK here is required to set the xnack+ setting for the GPU at runtime.
+# If it is not set and the default xnack setting of the system is xnack-
+# a runtime error something like "No kernel image found" will occur. The system
+# xnack setting can be found through rocminfo. xnack+ is required for ASAN.
+# More information about xnack in general can be found here:
+# https://llvm.org/docs/AMDGPUUsage.html#target-features
+# https://rocm.docs.amd.com/en/docs-6.1.0/conceptual/gpu-memory.html
+export HSA_XNACK=1
+
+# Disable buffer ops given it has builtin support for out of bound access.
+export AMDGCN_USE_BUFFER_OPS=0
 
 ASAN_OPTIONS=detect_leaks=0,alloc_dealloc_mismatch=0 \
-LD_PRELOAD=$CLANG_ASAN_LIB:$HIP_ASAN_LIB python test_asan_wrapper.py
+LD_PRELOAD=$CLANG_ASAN_LIB:$HIP_ASAN_LIB python test_asan.py
